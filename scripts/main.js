bfb7b42986f8660f92e2754c7062cef0fe7b05c8
david-y-e <58967641+david-y-e@users.noreply.github.com>
2020-02-03 22:04:27 +0900

    update main page
    update main page

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 function openNav() {
 
-  if(document.getElementById("mySidenav").style.width!="200px"){
-    document.getElementById("mySidenav").style.width = "200px";
-    document.getElementById("container").style.filter = "contrast(50%)";
+  if(document.getElementById("mySidenav").style.width!="250px"){
+    document.getElementById("mySidenav").style.width = "250px";
+    // document.getElementById("container").style.filter = "contrast(50%)";
 
   } else {
     document.getElementById("mySidenav").style.width = "0px";
-    document.getElementById("container").style.filter = "contrast(100%)";
+    // document.getElementById("container").style.filter = "contrast(100%)";
       }
 }
 
